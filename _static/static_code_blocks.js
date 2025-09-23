9d95c1986d025c0418acf91b1ddde28236154c23
reneaas <reneaas@uio.no>
2025-09-24 00:54:19 +0200

    update

--- _static/static_code_blocks.js
+++ _static/static_code_blocks.js
@@ -38,6 +38,16 @@ document.addEventListener("DOMContentLoaded", function() {
             block.style.color = '#ff0000';
             block.style.fontWeight = 'bold';
         }
+
+        if (block.textContent.trim().startsWith('# ????')) {
+            block.style.color = '#ff0000';
+            block.style.fontWeight = 'bold';
+        }
+
+        if (block.textContent.trim().startsWith('????')) {
+            block.style.color = '#ff0000';
+            block.style.fontWeight = 'bold';
+        }
     });
 });
 
